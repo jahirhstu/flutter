925204539d650191d833dd3280e73d87f29ea6cb
jahirhstu <jahirulislam@mac.local>
2018-11-19 01:06:58 +0600

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: FlutterLogo(colors: Colors.green, size: 25),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+          color: Colors.grey,
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.grey,
+          )
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 100,
+                color: Colors.white,
+                alignment: Alignment.center,
+                child: Text(
+                  'Get coaching',
+                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 2.0,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                child: Row(children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
+                        child: Text(
+                          'YOU HAVE',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(25.0, 40.0, 5.0, 25.0),
+                        child: Text(
+                          '206',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 40),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Container(
+                    //height: 60,
+                    //width: 125,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.fromLTRB(5.0, 25.0, 25.0, 25.0),
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent[100].withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Buy more',
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
