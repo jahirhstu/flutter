072b5019486e00dabdc9a0fc034e71d969118650
jahirhstu <jahirulislam@mac.local>
2018-11-21 15:30:40 +0600

    cards added.

--- lib/main.dart
+++ lib/main.dart
@@ -47,16 +47,16 @@ class _MyHomePageState extends State<MyHomePage> {
           Stack(
             children: <Widget>[
               Container(
-                height: 100,
+                height: 60,
                 color: Colors.white,
-                alignment: Alignment.center,
+                alignment: Alignment.topCenter,
                 child: Text(
                   'Get coaching',
                   style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
                 ),
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                margin: EdgeInsets.fromLTRB(25.0, 40.0, 25.0, 0.0),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20.0),
                   color: Colors.white,
@@ -100,7 +100,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   Container(
                     height: 60,
                     width: 125,
-                    alignment: Alignment.center,
                     padding: EdgeInsets.fromLTRB(5.0, 25.0, 25.0, 25.0),
                     decoration: BoxDecoration(
                       color: Colors.greenAccent[100].withOpacity(0.5),
@@ -121,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-          SizedBox(height: 40),
+          SizedBox(height: 10),
           Container(
             padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
             child: Row(
@@ -146,7 +145,87 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-          SizedBox(height: 20),
+          GridView.count(
+            primary: false,
+            crossAxisCount: 2,
+            mainAxisSpacing: 6.0,
+            crossAxisSpacing: 2.0,
+            shrinkWrap: true,
+            children: <Widget>[
+              _buildCard(),
+              _buildCard(),
+              _buildCard(),
+              _buildCard(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildCard() {
+    return Card(
+      margin: EdgeInsets.only(left: 25.0, right: 25.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      elevation: 1,
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Column(
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      'https://firebasestorage.googleapis.com/v0/b/autobazar-5fc52.appspot.com/o/rsz_mdabdullah_al_roman.jpg?alt=media&token=039c4847-1972-405d-9ea0-23348694f054',
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Abdullah',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      'Available for the next five hours.',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Text('Request'),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
